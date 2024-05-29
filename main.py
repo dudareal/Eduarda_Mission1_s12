fa2ecb02f980ebd160a7b76bd6e146b18e7eb822
Eduarda Real <dudarealferreira07@gmail.com>
2024-05-29 17:50:01 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def menu_principal():
+    print("/n** Calculadora de conversão de unidades**")
+    print("1. Converter Comprimento")
+    print("3. Sair")
+
+    opçao = input("Digite a opção desejada:")
+    return opçao   
+
+def sair():
+    print("Encerrando o programa...")
+
+escolha = menu_principal()
+
+if escolha == '3':
+    sair()
